4d9a62cc1ea6649752fafff820a8a25f07f1e8f8
Swayam-Shree <61933177+Swayam-Shree@users.noreply.github.com>
2024-02-13 21:47:09 +0530

    flunder domain

--- src/firebase/main.js
+++ src/firebase/main.js
@@ -3,13 +3,22 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCCwd8S-z1aYiHQdfR9bRg1DFIcbsHVHLE",
+//   authDomain: "flunder-787d0.firebaseapp.com",
+//   projectId: "flunder-787d0",
+//   storageBucket: "flunder-787d0.appspot.com",
+//   messagingSenderId: "1013887551567",
+//   appId: "1:1013887551567:web:cb1f025240b3658832823a"
+// };
+
 const firebaseConfig = {
   apiKey: "AIzaSyCCwd8S-z1aYiHQdfR9bRg1DFIcbsHVHLE",
   authDomain: "flunder-787d0.firebaseapp.com",
   projectId: "flunder-787d0",
   storageBucket: "flunder-787d0.appspot.com",
   messagingSenderId: "1013887551567",
-  appId: "1:1013887551567:web:cb1f025240b3658832823a"
+  appId: "1:1013887551567:web:633787659158087532823a"
 };
 
 const app = initializeApp(firebaseConfig);
